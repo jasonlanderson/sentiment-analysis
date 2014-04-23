c0992c4b777970c2201a131ac548a9fc358e4bdc
Curtis d'Entremont <curtispd@ca.ibm.com>
2014-04-23 00:28:48 -0400

    fixes

--- tasks/process-manifest.js
+++ tasks/process-manifest.js
@@ -1,6 +1,17 @@
-var grunt = require("grunt");
+var crypto = require('crypto'),
+	fs = require("fs"),
+	grunt = require("grunt");
 
+/*
+ * Processes the manifest.yml file, replacing ${random-word} with a random word.
+ */
 module.exports = function() {
-	grunt.log.write("hello grunt log");
-	console.log(__dirname);
+	// create a small hash of the current directory, which includes project and pipeline UUID
+	var hash = crypto.createHash('md5').update(__dirname).digest('hex'),
+		randomWord = (hash + "").substring(0, 4),
+		manifest = fs.readFileSync("manifest.yml", { encoding: "utf-8" });
+	
+	// replace ${random-word} in the manifest with the hash
+	manifest = manifest.replace("${random-word}", randomWord);
+	fs.writeFileSync("manifest.yml", manifest);
 };
